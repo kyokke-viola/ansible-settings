026259d0df30b3e265e961c98a624843860ea530
Kyosuke Matsumoto <kyokke@gmail.com>
2015-05-06 19:51:03 +0900

    confirm ansible-playbook can be called and add role/lang

--- trusty64/provision.sh
+++ trusty64/provision.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-echo "provision.sh is executed!!"
 # proxy_conf="/vagrant/ansible/roles/proxy/vars/main.yml"
 # apt_conf="/etc/apt/apt.conf.d/80proxy"
-# ansible_conf="/home/vagrant/.ansible.cfg"
+ansible_conf="/home/vagrant/.ansible.cfg"
 #
 # sed -i -e 's/\r$//' $proxy_conf
 #
@@ -21,15 +20,15 @@ echo "provision.sh is executed!!"
 #   sudo touch $apt_conf
 #   echo "Acquire::http::Proxy \"${http_proxy}\";" | sudo tee -a $apt_conf
 # fi
-# if ! [ `which ansible` ]; then
-#   sudo apt-get update -y
-#   sudo apt-get install -y ansible
-# fi
+if ! [ `which ansible` ]; then
+  sudo apt-get update -y
+  sudo apt-get install -y ansible
+fi
 #
-# if ! [ -e $ansible_conf ]; then
-#   touch $ansible_conf
-#   echo "[defaults] " >> $ansible_conf
-#   echo "log_path=/vagrant/ansible.log" >> $ansible_conf
-# fi
-#
-# ansible-playbook -i /vagrant/ansible/hosts /vagrant/ansible/playbook.yml -v
+if ! [ -e $ansible_conf ]; then
+  touch $ansible_conf
+  echo "[defaults] " >> $ansible_conf
+  echo "log_path=/vagrant/ansible.log" >> $ansible_conf
+fi
+# 
+ansible-playbook -i /vagrant/ansible/hosts /vagrant/ansible/playbook.yml -v
